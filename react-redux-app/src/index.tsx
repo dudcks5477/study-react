87473910f1e5e5fa5e420e77f72bd3ae606914d0
dudcks5477 <rladudcks5477@naver.com>
2024-02-23 11:51:03 +0900

    Provider

--- react-redux-app/src/index.tsx
+++ react-redux-app/src/index.tsx
@@ -5,13 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createStore} from 'redux';
 import rootReducer from './reducers';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 const store = createStore(rootReducer)
-
+ 
 store.dispatch({
   type: "ADD_TODO",
   text: "USE REDUX"
@@ -19,11 +20,13 @@ store.dispatch({
 console.log('store.getState', store.getState())
 const render = () => root.render(
   <React.StrictMode>
-    <App 
-      value={store.getState()}
-      onIncrement={() => store.dispatch({type: "INCREMENT"})}
-      onDecrement={() => store.dispatch({type: "DECREMENT"})}
-    />
+    <Provider store={store} >
+      <App 
+        value={store.getState()}
+        onIncrement={() => store.dispatch({type: "INCREMENT"})}
+        onDecrement={() => store.dispatch({type: "DECREMENT"})}
+      />
+    </Provider>
   </React.StrictMode>
 );
 render()
